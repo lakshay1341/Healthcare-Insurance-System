e03c380e03972b3ea33f755bde7f3065e90017f6
Lakshay <lakshaychaudhary2003@gmail.com>
2025-03-31 15:17:53 +0530

    commit: Correspondence service

--- ISH-CorrespondenceService/src/main/java/in/lakshay/ISHCorrespondenceServiceApplication.java
+++ ISH-CorrespondenceService/src/main/java/in/lakshay/ISHCorrespondenceServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CorrespondenceServiceApplication {
+public class ISHCorrespondenceServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CorrespondenceServiceApplication.class, args);
+        SpringApplication.run(ISHCorrespondenceServiceApplication.class, args);
     }
 
 }
