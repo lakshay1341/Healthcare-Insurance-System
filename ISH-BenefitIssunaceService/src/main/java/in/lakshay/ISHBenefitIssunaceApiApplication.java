b2428af11bd732eafef71be29767f253f1c25417
Lakshay <lakshaychaudhary2003@gmail.com>
2025-04-10 10:05:35 +0530

    commit: Benefit Insurance service

---
+++ ISH-BenefitIssunaceService/src/main/java/in/lakshay/ISHBenefitIssunaceApiApplication.java
@@ -0,0 +1,15 @@
+package in.lakshay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ISHBenefitIssunaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISHBenefitIssunaceApiApplication.class, args);
+	}
+
+}
